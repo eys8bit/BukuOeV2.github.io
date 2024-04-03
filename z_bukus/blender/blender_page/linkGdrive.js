9be3f59eb0e682d12df1ebcf0090024f338f644f
EYS06 <elyas.achmadudin30@gmail.com>
2024-04-03 21:37:40 +0700

    update

---
+++ z_bukus/blender/blender_page/linkGdrive.js
@@ -0,0 +1,19 @@
+ //membarikan link google drive alamat lewat id src=  
+ var jsonData = {
+    // video_1:
+    //   "https://drive.google.com/file/d/1gILTAeQTthF3geDBs7sS44lYahwas9xP/preview",
+    // gambar_2:
+    //   "https://drive.google.com/thumbnail?id=1__gZlUvX5aZZ8jlJrR1oUU_SI1Sl8_TK",
+    gambar_1:
+      "https://drive.google.com/thumbnail?id=18liDkBZ7Zn1r48o5h1l8Iq0dJVorf5ZL",
+  };
+
+  // Loop untuk semua iframe dengan id yang dimulai dengan "video_"
+  for (var key in jsonData) {
+    if (jsonData.hasOwnProperty(key)) {
+      var linkKey = document.getElementById(key);
+      if (linkKey) {
+        linkKey.src = jsonData[key];
+      }
+    }
+  }
